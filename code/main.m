baad49ddd89389d362574bd900c9caccaf6626fb
Mohammad Farzan <m2_farzan@yahoo.com>
2021-07-21 16:47:04 +0430

    Add logging

--- code/main.m
+++ code/main.m
@@ -2,11 +2,15 @@ clc
 clear
 clear all
 
+disp('Initializing...')
+
 load_plant_params;
 load_controller_params;
 
 [X_0, W_0] = load_initial_state();
 S_0 = [X_0, W_0];
+
+disp('Simulating...');
 
 tic;
     [t, S] = ode45(@Sdot, [0, 60], S_0);
@@ -15,4 +19,8 @@ toc;
 X = S(:, 1:7);
 W = S(:, 8:18);
 
+disp('Generating plots...')
+
 visualize_results(t, X, W);
+
+disp('Process finished.')
