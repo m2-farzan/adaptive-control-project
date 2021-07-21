443711c83586409ea0839691d0bb93e22d3abbc7
Mohammad Farzan <m2_farzan@yahoo.com>
2021-07-21 16:44:29 +0430

    Generate plots to compare with the article

--- code/visualize_results.m
+++ code/visualize_results.m
@@ -1,18 +1,94 @@
 function visualize_results(t, X, W)
+    % Close all open figures
+    close all;
+
+    % Load some global vars (Used to calculate S_13)
+    global k_1
+    
+    % Calculate reference velocities (Used to calculate S_13)
+    v_r = zeros(size(t, 1), 1);
+    for i =  1:size(t, 1)
+        z = z_r(t(i));
+        v_r(i) = z(1);
+    end
+
+    % Unpack plant states
     x = X(:, 1);
     y = X(:, 2);
-    i_a_1 = X(:, 6);
-    i_a_2 = X(:, 7);
 
-    figure();
+    % Unpack controller states
+    a_hat_1 = W(:, 1);
+    a_hat_2 = W(:, 2);
+    a_hat_3 = W(:, 3);
+    a_hat_4 = W(:, 4);
+    S_11 = W(:, 9);
+    S_12 = W(:, 10);
+    S_bar_13 = W(:, 11);
+    S_13 = S_bar_13 + atan(k_1 * S_12 .* v_r);
+    
+    % Get controller inputs
+    u = zeros(size(t, 1), 2);
+    for i =  1:size(t, 1)
+        [~, u(i, :)] = Wdot(X(i, :)', W(i, :)', z_r(t(i)));
+    end
+
+    % Draw figures
+    % Fig 2.A
+    fig = figure();
     plot(x, y);
-    xlabel('x');
-    ylabel('y');
+    xlabel('X-axis (m)');
+    ylabel('Y-axis (m)');
+    saveas(fig, 'fig-2A-xy.eps', 'epsc');
+    
+    % Fig 2.B
+    fig = figure();
+    plot(t, S_11);
+    xlabel('t (s)');
+    ylabel('S_{11} tracking error (m)');
+    saveas(fig, 'fig-2B-s11.eps', 'epsc');
 
-    figure();
-    hold on;
-    plot(t, i_a_1, 'DisplayName', 'i_a1');
-    plot(t, i_a_2, 'DisplayName', 'i_a2');
-    legend();
-    xlabel('t');
+    % Fig 2.C
+    fig = figure();
+    plot(t, S_12);
+    xlabel('t (s)');
+    ylabel('S_{12} tracking error (m)');
+    saveas(fig, 'fig-2C-s12.eps', 'epsc');
+    
+    % Fig 2.D
+    fig = figure();
+    plot(t, S_13);
+    xlabel('t (s)');
+    ylabel('S_{13} tracking error (rad)');
+    saveas(fig, 'fig-2D-s13.eps', 'epsc');
+    
+    % Fig 3.A
+    fig = figure();
+    plot(t, u);
+    xlabel('t (s)');
+    ylabel('Control inputs (V)');
+    legend('Right wheel', 'Left wheel');
+    saveas(fig, 'fig-3A-v.eps', 'epsc');
+    
+    % Fig 3.B
+    fig = figure();
+    plot(t, [a_hat_1, a_hat_2]);
+    xlabel('t (s)');
+    ylabel('Estimated a_1 and a_2');
+    legend('a_1', 'a_2');
+    saveas(fig, 'fig-3B-a1-a2.eps', 'epsc');
+    
+    % Fig 3.C
+    fig = figure();
+    plot(t, a_hat_3);
+    xlabel('t (s)');
+    ylabel('Estimated a_3');
+    saveas(fig, 'fig-3C-a3.eps', 'epsc');
+    
+    % Fig 3.D
+    fig = figure();
+    plot(t, a_hat_4);
+    xlabel('t (s)');
+    ylabel('Estimated a_4');
+    saveas(fig, 'fig-3D-a4.eps', 'epsc');
+
 end
